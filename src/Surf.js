044f63cb7d62b5847df6d8329ff396c8beb40f26
ryan leslie <ryanleslie33@aol.com>
2019-03-20 16:15:49 -0700

    complete all functionality with dom and html working properly

--- src/Surf.js
+++ src/Surf.js
@@ -1,6 +1,6 @@
 export class Surf {
   
-  constructor(swellPeriod,waveSize,windDirection,swellHeight){
+  constructor(windDirection,swellPeriod,waveSize,swellHeight){
     this.wind = windDirection;
     this.size = waveSize;
     this.swell = swellPeriod;
@@ -8,22 +8,21 @@ export class Surf {
     this.windScore = 0;
     this.sizeScore = 0;
     this.swellScore = 0;
-    
   }
   
   swellChecker(){
     let swell = this.swell
     if(swell > 16){
-      this.swellScore == 5;
+      this.swellScore += 5;
     }
-    else if(swell >= 12 && <= 15){
-      this.swellScore == 4;
+    else if (swell >= 12 && swell <= 15) {
+      this.swellScore += 4;
     }
-    else if (swell >= 10 && <=12 ) {
-      this.swellScore == 3;
+    else if (swell >= 10 && swell <=12 ) {
+      this.swellScore += 3;
     }
     else if(swell < 10){
-      this.swellScore == 1;
+      this.swellScore += 1;
     }
     return this.swellScore;
   }
@@ -32,27 +31,28 @@ export class Surf {
     let height = this.swellHeight
     let swellHeight = (swell * height);
     if ( swellHeight >= 30){
-      this.sizeScore == 5;
+      this.sizeScore += 5;
     }
-  else if ( swellHeight >= 25 && <= 30){
-      this.sizeScore == 4;
+  else if ( swellHeight >= 25 && swellHeight <= 30){
+      this.sizeScore += 4;
     }
-    else if ( swellHeight >= 20 && <= 24){
-        this.sizeScore == 3;
+    else if ( swellHeight >= 20 && swellHeight <= 24){
+        this.sizeScore += 3;
       }
-      else if ( swellHeight >= 11 && <= 19){
-          this.sizeScore == 2;
+      else if ( swellHeight >= 11 && swellHeight <= 19){
+          this.sizeScore += 2;
         }
         else if ( swellHeight <10){
-            this.sizeScore == 1;
+            this.sizeScore += 1;
           }
           return this.sizeScore;
   }
   calculateScore(){
-    let wind = this.windDirection;
+    let wind = this.wind;
     let sizeScore = this.sizeScore;
     let swellScore = this.swellScore;
-    let answer = ( wind + sizeScore + swellScore);
-    return answer:
+    let answer = ( wind + sizeScore + swellScore / 3);
+  
+    return answer;
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import $ from 'jquery';
+import  { Surf } from 'C:/Users/ryan leslie/Desktop/Surf-Report/src/Surf.js'
+
 $(document).ready(function() {
   $("#form").submit(function(event){
     event.preventDefault()
@@ -5,5 +8,11 @@ $(document).ready(function() {
     let swellPeriod = $("#swellPeriod").val();
     let waveSize = $("#waveSize").val();
     let swellHeight = $("#swellHeight").val();
+  
+    let newSurf = new Surf(windDirection,swellPeriod,waveSize,swellHeight)
+    newSurf.swellChecker();
+    newSurf.sizeChecker();
+    newSurf.calculateScore();
+    $("#output").append(newSurf.calculateScore());
   })
 })
