f77978abe575289f2b90f2bc4a1407dea4621a54
ryan leslie <ryanleslie33@aol.com>
2019-03-20 13:22:57 -0700

    add initial set up page, npm install no errors

---
+++ src/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+$("#form").submit(function(event){
+event.preventDefault()
+let windDirection = $("#windDirection").val();
+ let swellPeriod = $("#swellPeriod").val();
+ let waveSize = $("#waveSize").val();
+}
+}
