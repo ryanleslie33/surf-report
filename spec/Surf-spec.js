858708d908e9da71ed5f2e0d777e0e8a8613ec64
ryan leslie <ryanleslie33@aol.com>
2019-03-20 17:51:37 -0700

    complete all test and functionality

---
+++ spec/Surf-spec.js
@@ -0,0 +1,24 @@
+import  { Surf }  from '../src/Surf.js';
+
+describe("Surf", function() {
+  let newSurf;
+  
+  beforeEach(function() {
+    newSurf = new Surf(3,3,3,3);
+    newSurf.sizeChecker()
+    newSurf.swellChecker()
+  });
+  
+  it ("determines the overal score report", function() {
+    
+    expect(newSurf.calculateScore()).toEqual(4.333333333333333)
+  });
+  it ("determines the size checker function with input of 3", function() {
+    
+    expect(newSurf.sizeChecker()).toEqual(2)
+  });
+  it ("determines the size checker function with input of 3", function() {
+    
+    expect(newSurf.swellChecker()).toEqual(2)
+  });
+})
--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import  { Surf } from 'C:/Users/ryan leslie/Desktop/Surf-Report/src/Surf.js'
+
 
 $(document).ready(function() {
   $("#form").submit(function(event){
     event.preventDefault()
-    let windDirection = $("#windDirection").val();
-    let swellPeriod = $("#swellPeriod").val();
-    let waveSize = $("#waveSize").val();
-    let swellHeight = $("#swellHeight").val();
+    let windDirection = parseFloat($("#windDirection").val());
+    let swellPeriod = parseFloat($("#swellPeriod").val());
+    let waveSize = parseFloat($("#waveSize").val());
+    let swellHeight = parseFloat($("#swellHeight").val());
   
     let newSurf = new Surf(windDirection,swellPeriod,waveSize,swellHeight)
+    let time = new Date()
     newSurf.swellChecker();
     newSurf.sizeChecker();
     newSurf.calculateScore();
-    $("#output").append(newSurf.calculateScore());
+    $("#output").append( "your overall score is " +  newSurf.calculateScore() + ": "  + time);
   })
 })
